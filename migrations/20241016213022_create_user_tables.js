204c0687029330120f131e02eaa8876a9e2e4a7a
Sharan Athiyadath <sharanadhi@gmail.com>
2024-10-16 19:00:09 -0300

    Initial commit

---
+++ migrations/20241016213022_create_user_tables.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema
+    .createTable("users", (table) => {
+      table.increments("id").primary();
+      table.string("full_name").notNullable();
+      table.string("email").notNullable();
+      table.string("phone",10).notNullable();
+      table.string("password").notNullable();
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+    })
+}
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable("users");
+};
---
+++ seed_data/users.js
@@ -0,0 +1,9 @@
+export default [
+  {
+    id: 1,
+    full_name: "Sharan Athiyadath",
+    email: "sharanadhi@gmail.com",
+    phone:"7824093459",
+    password:"123!@#Poi"    
+  }  
+];
