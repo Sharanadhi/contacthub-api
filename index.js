8ad03cc85ecc3b1e58389b5d6ac3520a7b78ecef
Sharan Athiyadath <sharanadhi@gmail.com>
2024-10-16 19:22:12 -0300

    Added user routes for user related apis

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import "dotenv/config";
 import express from "express";
+
+import userRoutes from './routes/user-routes.js';
+
 const app = express();
 
 const PORT = process.env.PORT || 5050;
@@ -7,6 +10,8 @@ const PORT = process.env.PORT || 5050;
 app.get("/", (req, res) => {
   res.send("Welcome to my API");
 });
+
+app.use('/api/users', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`running at http://localhost:${PORT}`);
