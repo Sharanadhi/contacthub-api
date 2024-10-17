f3844611df95a0dece6d006a6dd01d2c0417c179
Sharan Athiyadath <sharanadhi@gmail.com>
2024-10-17 03:18:17 -0300

    updated email configuration

--- routes/signin-signup-routes.js
+++ routes/signin-signup-routes.js
@@ -93,6 +93,8 @@ router.post("/signin", async (req, res) => {
 
 // Generate Password Reset Token API
 router.post("/reset-password", async (req, res) => {
+  const token = crypto.randomBytes(20).toString('hex');
+  const tokenExpiration = moment().add(1, 'hour').format('YYYY-MM-DD HH:mm:ss'); 
   const { email } = req.body;
   try {
     const user = await knex("users").where({ email }).first();
@@ -101,8 +103,8 @@ router.post("/reset-password", async (req, res) => {
     }
 
     // Generate a token
-    const token = crypto.randomBytes(20).toString('hex');
-    const tokenExpiration = moment().add(1, 'hour').format('YYYY-MM-DD HH:mm:ss'); 
+    // const token = crypto.randomBytes(20).toString('hex');
+    // const tokenExpiration = moment().add(1, 'hour').format('YYYY-MM-DD HH:mm:ss'); 
 
     await knex("users")
       .where({ email })
@@ -110,12 +112,13 @@ router.post("/reset-password", async (req, res) => {
 
     // Send the email with the token
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
+        host: "sandbox.smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD
+        }
+      });
 
     const mailOptions = {
       from: process.env.EMAIL_USERNAME,
@@ -124,7 +127,16 @@ router.post("/reset-password", async (req, res) => {
       text: `You requested a password reset. Click this link to reset your password: http://localhost:5173/reset/${token}`
     };
 
-    transporter.sendMail(mailOptions);
+    console.log('Sending email to:', email);
+console.log('Mail options:', mailOptions);
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);
+    });
+    
 
     res.status(200).json({ message: "Password reset link sent",token });
   } catch (error) {
